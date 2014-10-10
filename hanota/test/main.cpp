0035d4d14afa809e2ab42a4a059ae8ff800d2db3
黄琦伟 <huangqiwei@huangqiweideMacBook-Air.local>
2014-10-10 20:44:20 +0800

    add makefile

--- hanota/test/main.cpp
+++ hanota/test/main.cpp
@@ -18,7 +18,7 @@ int main()
     cout << "test case 1:" << endl;
     int disc[] = {0, 1, 2};
     int pilar[] = {0, 1, 2};
-    hanota(disc, disc + 3, pilar[0], pilar[2], pilar[1], move());
+    hanota(disc, disc + 3, pilar[0], pilar[2], pilar[1], ::move());
 
     cout << "test case 2:" << endl;
     vector<string> disc2;
@@ -35,5 +35,5 @@ int main()
 	p << "pilar #" << i;
 	pilar2.push_back(p.str());
     }
-    hanota(disc2.begin(), disc2.end(), pilar2[0], pilar2[2], pilar2[1], move());
+    hanota(disc2.begin(), disc2.end(), pilar2[0], pilar2[2], pilar2[1], ::move());
 }
